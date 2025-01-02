cfee461df012e132eef82b3523ddb2f57c77a4e0
dameng <damengcoder@gmail.com>
2025-01-02 14:52:55 +0800

    init

---
+++ src/main/index.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true,
+        },
+    });
+
+    mainWindow.loadFile(path.join(__dirname, '../uniapp/dist/build/h5/index.html'));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
---
+++ src/uniapp/vue.config.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+module.exports = {
+    outputDir: path.resolve(__dirname, '../../public/uniapp'), // 指定输出目录
+    publicPath: './', // 使用相对路径
+};
